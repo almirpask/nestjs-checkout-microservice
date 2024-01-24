35a3c1d1fa19f80c854f77d009759706576342cf
almirpask <almirpask@hotmail.com>
2024-01-23 23:38:23 -0300

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CheckoutsModule } from './checkouts/checkouts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Checkout } from './checkouts/entities/checkout.entity';
 
 @Module({
-  imports: [CheckoutsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Checkout],
+      synchronize: true,
+      logging: true,
+    }),
+    CheckoutsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
